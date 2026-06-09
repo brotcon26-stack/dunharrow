9125062c50d9d2e0710cad02bdb176d3acc6d97d
brotcon26 <brotcon26@gmail.com>
2026-06-08 22:15:54 -0700

    made build.sh able to bild rawhide images

--- build.sh
+++ build.sh
@@ -1,2 +1,16 @@
-podman build -f Containerfile.stable -t registry.lab/dunharrow:latest \
-&& podman push registry.lab/dunharrow:latest
+#!/bin/bash
+TARGET=${1:-'stable'}
+
+if [ "$TARGET" = "rawhide" ]; then
+    podman build -t registry.lab/dunharrow:rawhide \
+        -f Containerfile.stable \
+        --build-arg BASE_IMAGE=quay.io/fedora/fedora-bootc:rawhide \
+        && podman push registry.lab/dunharrow:rawhide
+
+else
+    podman build -t registry.lab/dunharrow:stable \
+        -f Containerfile.stable \
+        && podman push registry.lab/dunharrow:stable
+
+fi
+
